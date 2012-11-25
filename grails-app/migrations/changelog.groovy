9871edf5b0a96602321f2170e065fdab972bdaa0
Andrew Bayer <andrew.bayer@gmail.com>
2012-11-25 12:26:39 -0800

    Removing index creation from changelog to work properly with mysql.

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -1,6 +1,6 @@
 databaseChangeLog = {
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-1") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-1") {
 		createTable(tableName: "alert") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "alertPK")
@@ -26,7 +26,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-2") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-2") {
 		createTable(tableName: "audit_log") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "audit_logPK")
@@ -60,7 +60,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-3") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-3") {
 		createTable(tableName: "cloud_stack_config") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "cloud_stack_cPK")
@@ -98,7 +98,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-4") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-4") {
 		createTable(tableName: "cpu_usage") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "cpu_usagePK")
@@ -118,7 +118,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-5") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-5") {
 		createTable(tableName: "host") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "hostPK")
@@ -280,7 +280,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-6") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-6") {
 		createTable(tableName: "instance") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "instancePK")
@@ -434,7 +434,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-7") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-7") {
 		createTable(tableName: "logging") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "loggingPK")
@@ -460,7 +460,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-8") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-8") {
 		createTable(tableName: "notify_down_host") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "notify_down_hPK")
@@ -486,7 +486,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-9") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-9") {
 		createTable(tableName: "notify_seen_alerts") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "notify_seen_aPK")
@@ -502,7 +502,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-10") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-10") {
 		createTable(tableName: "provisioned_instance") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "provisioned_iPK")
@@ -536,7 +536,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-11") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-11") {
 		createTable(tableName: "provisioned_instance_group") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "provisioned_iPK")
@@ -606,7 +606,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-12") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-12") {
 		createTable(tableName: "registration_code") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "registration_PK")
@@ -626,7 +626,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-13") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-13") {
 		createTable(tableName: "report_run") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "report_runPK")
@@ -646,31 +646,31 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-14") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-14") {
 		createTable(tableName: "report_run_hosts") {
-			column(name: "report_run_id", type: "bigint") {
-				constraints(nullable: "false")
-			}
-
 			column(name: "host_id", type: "bigint") {
 				constraints(nullable: "false")
 			}
-		}
-	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-15") {
-		createTable(tableName: "report_run_instances") {
 			column(name: "report_run_id", type: "bigint") {
 				constraints(nullable: "false")
 			}
+		}
+	}
 
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-15") {
+		createTable(tableName: "report_run_instances") {
 			column(name: "instance_id", type: "bigint") {
+				constraints(nullable: "false")
+			}
+
+			column(name: "report_run_id", type: "bigint") {
 				constraints(nullable: "false")
 			}
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-16") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-16") {
 		createTable(tableName: "sec_role") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "sec_rolePK")
@@ -686,7 +686,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-17") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-17") {
 		createTable(tableName: "sec_user") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "sec_userPK")
@@ -720,7 +720,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-18") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-18") {
 		createTable(tableName: "sec_user_sec_role") {
 			column(name: "sec_role_id", type: "bigint") {
 				constraints(nullable: "false")
@@ -732,7 +732,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-19") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-19") {
 		createTable(tableName: "service_offering") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "service_offerPK")
@@ -812,7 +812,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-20") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-20") {
 		createTable(tableName: "spring_security_event") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "spring_securiPK")
@@ -836,7 +836,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-21") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-21") {
 		createTable(tableName: "template") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
 				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "templatePK")
@@ -964,177 +964,76 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-22") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-22") {
 		addPrimaryKey(columnNames: "report_run_id, host_id", tableName: "report_run_hosts")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-23") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-23") {
 		addPrimaryKey(columnNames: "report_run_id, instance_id", tableName: "report_run_instances")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-24") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-24") {
 		addPrimaryKey(columnNames: "sec_role_id, sec_user_id", constraintName: "sec_user_sec_PK", tableName: "sec_user_sec_role")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-25") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-25") {
 		addForeignKeyConstraint(baseColumnNames: "instance_id", baseTableName: "cpu_usage", constraintName: "FK9EF6844AB96091D5", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "instance", referencesUniqueColumn: "false")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-26") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-26") {
 		addForeignKeyConstraint(baseColumnNames: "host_id", baseTableName: "instance", constraintName: "FK21169495383AEA75", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "host", referencesUniqueColumn: "false")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-27") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-27") {
 		addForeignKeyConstraint(baseColumnNames: "service_offering_id", baseTableName: "instance", constraintName: "FK21169495E14882AA", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "service_offering", referencesUniqueColumn: "false")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-28") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-28") {
 		addForeignKeyConstraint(baseColumnNames: "template_id", baseTableName: "instance", constraintName: "FK2116949530494BB5", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "template", referencesUniqueColumn: "false")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-29") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-29") {
 		addForeignKeyConstraint(baseColumnNames: "provisioned_instance_group_id", baseTableName: "provisioned_instance", constraintName: "FKE7227C401ABAE94B", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "provisioned_instance_group", referencesUniqueColumn: "false")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-30") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-30") {
 		addForeignKeyConstraint(baseColumnNames: "alternate_service_offering_id", baseTableName: "provisioned_instance_group", constraintName: "FKEBE1A5C0F92F4F65", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "service_offering", referencesUniqueColumn: "false")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-31") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-31") {
 		addForeignKeyConstraint(baseColumnNames: "primary_service_offering_id", baseTableName: "provisioned_instance_group", constraintName: "FKEBE1A5C039B308ED", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "service_offering", referencesUniqueColumn: "false")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-32") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-32") {
 		addForeignKeyConstraint(baseColumnNames: "team_id", baseTableName: "provisioned_instance_group", constraintName: "FKEBE1A5C09C277F09", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "sec_role", referencesUniqueColumn: "false")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-33") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-33") {
 		addForeignKeyConstraint(baseColumnNames: "template_id", baseTableName: "provisioned_instance_group", constraintName: "FKEBE1A5C030494BB5", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "template", referencesUniqueColumn: "false")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-34") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-34") {
 		addForeignKeyConstraint(baseColumnNames: "host_id", baseTableName: "report_run_hosts", constraintName: "FK91B5E7CC383AEA75", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "host", referencesUniqueColumn: "false")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-35") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-35") {
 		addForeignKeyConstraint(baseColumnNames: "report_run_id", baseTableName: "report_run_hosts", constraintName: "FK91B5E7CC5A4CE096", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "report_run", referencesUniqueColumn: "false")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-36") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-36") {
 		addForeignKeyConstraint(baseColumnNames: "instance_id", baseTableName: "report_run_instances", constraintName: "FK9D3E3DFFB96091D5", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "instance", referencesUniqueColumn: "false")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-37") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-37") {
 		addForeignKeyConstraint(baseColumnNames: "report_run_id", baseTableName: "report_run_instances", constraintName: "FK9D3E3DFF5A4CE096", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "report_run", referencesUniqueColumn: "false")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-38") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-38") {
 		addForeignKeyConstraint(baseColumnNames: "sec_role_id", baseTableName: "sec_user_sec_role", constraintName: "FK6630E2A6ABF6062", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "sec_role", referencesUniqueColumn: "false")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-39") {
+	changeSet(author: "ubuntu (generated)", id: "1353875593146-39") {
 		addForeignKeyConstraint(baseColumnNames: "sec_user_id", baseTableName: "sec_user_sec_role", constraintName: "FK6630E2AFEA2442", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "sec_user", referencesUniqueColumn: "false")
 	}
 
-	changeSet(author: "abayer (generated)", id: "1353874063742-40") {
-		createIndex(indexName: "FK9EF6844AB96091D5", tableName: "cpu_usage") {
-			column(name: "instance_id")
-		}
-	}
-
-	changeSet(author: "abayer (generated)", id: "1353874063742-41") {
-		createIndex(indexName: "FK2116949530494BB5", tableName: "instance") {
-			column(name: "template_id")
-		}
-	}
-
-	changeSet(author: "abayer (generated)", id: "1353874063742-42") {
-		createIndex(indexName: "FK21169495383AEA75", tableName: "instance") {
-			column(name: "host_id")
-		}
-	}
-
-	changeSet(author: "abayer (generated)", id: "1353874063742-43") {
-		createIndex(indexName: "FK21169495E14882AA", tableName: "instance") {
-			column(name: "service_offering_id")
-		}
-	}
-
-	changeSet(author: "abayer (generated)", id: "1353874063742-44") {
-		createIndex(indexName: "FKE7227C401ABAE94B", tableName: "provisioned_instance") {
-			column(name: "provisioned_instance_group_id")
-		}
-	}
-
-	changeSet(author: "abayer (generated)", id: "1353874063742-45") {
-		createIndex(indexName: "FKEBE1A5C030494BB5", tableName: "provisioned_instance_group") {
-			column(name: "template_id")
-		}
-	}
-
-	changeSet(author: "abayer (generated)", id: "1353874063742-46") {
-		createIndex(indexName: "FKEBE1A5C039B308ED", tableName: "provisioned_instance_group") {
-			column(name: "primary_service_offering_id")
-		}
-	}
-
-	changeSet(author: "abayer (generated)", id: "1353874063742-47") {
-		createIndex(indexName: "FKEBE1A5C09C277F09", tableName: "provisioned_instance_group") {
-			column(name: "team_id")
-		}
-	}
-
-	changeSet(author: "abayer (generated)", id: "1353874063742-48") {
-		createIndex(indexName: "FKEBE1A5C0F92F4F65", tableName: "provisioned_instance_group") {
-			column(name: "alternate_service_offering_id")
-		}
-	}
-
-	changeSet(author: "abayer (generated)", id: "1353874063742-49") {
-		createIndex(indexName: "FK91B5E7CC383AEA75", tableName: "report_run_hosts") {
-			column(name: "host_id")
-		}
-	}
-
-	changeSet(author: "abayer (generated)", id: "1353874063742-50") {
-		createIndex(indexName: "FK91B5E7CC5A4CE096", tableName: "report_run_hosts") {
-			column(name: "report_run_id")
-		}
-	}
-
-	changeSet(author: "abayer (generated)", id: "1353874063742-51") {
-		createIndex(indexName: "FK9D3E3DFF5A4CE096", tableName: "report_run_instances") {
-			column(name: "report_run_id")
-		}
-	}
-
-	changeSet(author: "abayer (generated)", id: "1353874063742-52") {
-		createIndex(indexName: "FK9D3E3DFFB96091D5", tableName: "report_run_instances") {
-			column(name: "instance_id")
-		}
-	}
-
-	changeSet(author: "abayer (generated)", id: "1353874063742-53") {
-		createIndex(indexName: "authority_unique_1353874063660", tableName: "sec_role", unique: "true") {
-			column(name: "authority")
-		}
-	}
-
-	changeSet(author: "abayer (generated)", id: "1353874063742-54") {
-		createIndex(indexName: "username_unique_1353874063666", tableName: "sec_user", unique: "true") {
-			column(name: "username")
-		}
-	}
-
-	changeSet(author: "abayer (generated)", id: "1353874063742-55") {
-		createIndex(indexName: "FK6630E2A6ABF6062", tableName: "sec_user_sec_role") {
-			column(name: "sec_role_id")
-		}
-	}
-
-	changeSet(author: "abayer (generated)", id: "1353874063742-56") {
-		createIndex(indexName: "FK6630E2AFEA2442", tableName: "sec_user_sec_role") {
-			column(name: "sec_user_id")
-		}
-	}
 }
