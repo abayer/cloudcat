49440f4039fd02df6721d9cb008142f6f660ce98
Andrew Bayer <andrew.bayer@gmail.com>
2012-12-01 10:16:17 -0800

    Don't allow invalid instance names

--- grails-app/domain/cloudstack/reporting/ProvisionedInstanceGroup.groovy
+++ grails-app/domain/cloudstack/reporting/ProvisionedInstanceGroup.groovy
@@ -33,7 +33,7 @@ class ProvisionedInstanceGroup {
     static hasMany = [provisionedInstances:ProvisionedInstance]
     
     static constraints = {
-        shortName(matches:/(?i)^[a-z][a-z0-9\-]+[^\-]+$/,
+        shortName(matches:'[a-zA-Z][a-zA-Z0-9-]+[a-zA-Z0-9]',
                   validator: { val, obj -> return obj.noExistingInstances(obj.shortName, obj.primaryCount + obj.secondaryCount, obj.offset, obj.id) })
 
         alternateServiceOffering(nullable:true)
@@ -46,6 +46,7 @@ class ProvisionedInstanceGroup {
     }
 
     def noExistingInstances(testName, testCount, testOffset, testId) {
+
         for (cnt in 1..testCount) {
             def g = ProvisionedInstanceGroup.get(testId)
             if (ProvisionedInstance.findByInstanceNameLikeAndProvisionStatusNotEqualAndProvisionedInstanceGroupNotEqual("${testName}-${testOffset + cnt}%", 9, g)) {
